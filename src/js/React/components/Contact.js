3cae0488ddb65ff1e318604286966a645f8dca2c
mohfullstack <30382246+mohfullstack@users.noreply.github.com>
2017-07-25 06:38:35 -0400

    Update Contact.js

--- src/js/React/components/Contact.js
+++ src/js/React/components/Contact.js
@@ -5,12 +5,12 @@ const Contact = () => {
     <section className="contact" id="contactMe">
         <div className="contact__information">
           <h1 id="contact-header"> CONTACT </h1>
-          <h3> Pmarko.alt@gmail.com </h3>
+          <h3> mohkerate@gmail.com </h3>
           <div className="contact__information__svg">
-            <a href="https://github.com/Pmarkoalt" target="_blank"> <img src="images/devicons/github-original.svg"/> </a>
-            <a href="https://teamtreehouse.com/petermarkoski" target="_blank"> <img src="images/devicons/treehouse-logo.png"/> </a>
+            <a href="https://github.com/mohfullstack" target="_blank"> <img src="images/devicons/github-original.svg"/> </a>
+            
             <a href="https://stackoverflow.com/users/7340994/pmarko-alt" target="_blank"> <img src="images/devicons/stack.svg"/> </a>
-            <a href="https://www.linkedin.com/in/peter-markoski/" target="_blank"> <img src="images/devicons/linkedin-plain.svg"/> </a>
+            <a href="https://www.linkedin.com/in/reshid-kerate-24997010a/" target="_blank"> <img src="images/devicons/linkedin-plain.svg"/> </a>
           </div>
         </div>
 
