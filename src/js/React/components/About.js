7037ca0ccce9c21da0dae28fb030438837c432ca
mohfullstack <30382246+mohfullstack@users.noreply.github.com>
2017-07-25 06:35:01 -0400

    Update About.js

--- src/js/React/components/About.js
+++ src/js/React/components/About.js
@@ -7,7 +7,7 @@ const About = () => {
 
   <section className="about table" id="about">
     <h1 className="about__header" data-aos="fade-up"> ABOUT </h1>
-    <p className="about__text" data-aos="fade-up" > My name is Peter Markoski and I am a Full Stack Developer located in Washington DC. I like building websites that incorporate new technologies while maintaining high usability. I program primarily with HTML, CSS, and JavaScript. I also have experience with Wordpress, Octave, and various DSP languages. I am a massive audiophile who likes working with sounds whether it be mixing records, sound design, or producing songs. Needless to say, I am always within arms reach of a computer. </p>
+    <p className="about__text" data-aos="fade-up" > My name is Mohammed Kemal and I am a Full Stack Web Developer located in Washington DC. I like building websites that incorporate new technologies while maintaining high usability. I program primarily with HTML, CSS, and JavaScript. I also have experience with Wordpress, Octave, and various DSP languages. I am a massive audiophile who likes working with sounds whether it be mixing records, sound design, or producing songs. Needless to say, I am always within arms reach of a computer. </p>
 
   </section>
   );
